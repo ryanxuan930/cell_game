d15bfeabc7f1463b94da0d3a16ece13c75ca3fcd
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-12-24 00:53:55 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,25 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const width = ref(0);
+const height = ref(0);
+const iterations = ref(0);
+const mapLayer = ref([]);
+
+enum CellType {
+  Normal,
+  Water,
+  Sand,
+  Predator,
+  
+}
+
+type TCell = {
+  x: number;
+  y: number;
+  isAlive: boolean;
+  type: CellType;
+};
 </script>
 
 <template>
